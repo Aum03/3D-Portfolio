a8f69811a70245a3b0436f14bc39ecf96f6b9db2
Aum <aumyadav0311@gmail.com>
2025-01-19 04:38:58 +0530

    Updated the pub key

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -42,7 +42,7 @@ const Contact = () => {
           to_email: "aumyadav0311@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        "D08eAGEFQdOIZvpMt"
       )
       .then(
         () => {
